c530bf186bf99d91d9b3a29537ca55bb40ead600
bewayo2 <bewayo@gmail.com>
2025-07-24 21:29:17 -0400

    react start fix

--- server/app.js
+++ server/app.js
@@ -16,10 +16,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // API endpoint for scoring
